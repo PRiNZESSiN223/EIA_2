8ef14ee8c425dd6f65f1d0172c97ba7da341fee5
maxime <maxime@maximes-mbp.eduroam.hs-furtwangen.de>
2017-10-11 16:19:14 +0200

    xx

--- miniSeite/mini.js
+++ miniSeite/mini.js
@@ -1,7 +1,12 @@
 var name = prompt("Name");
 function init() {
     var node = document.getElementById("inhalt");
-    node.innerHTML = "WOW respekt an dich " + name + " du kannst deinen Namen schreiben.";
+    if (name.length < 1) {
+        node.innerHTML = "Weißt du nicht wie du heißt ?";
+    }
+    if (name.length >= 1) {
+        node.innerHTML = "WOW respekt an dich " + name + " du kannst deinen Namen schreiben.";
+    }
 }
 window.addEventListener("load", init);
 //# sourceMappingURL=mini.js.map
--- miniSeite/mini.ts
+++ miniSeite/mini.ts
@@ -1,9 +1,13 @@
-var name = prompt("Name");
+var name :string = prompt("Name");
 
 function init():void{
-  let node = document.getElementById("inhalt");
+      let node = document.getElementById("inhalt");
+    if(name.length < 1){
+    node.innerHTML = "Weißt du nicht wie du heißt ?";
+}
+        if(name.length >= 1){
     node.innerHTML = "WOW respekt an dich "+name+" du kannst deinen Namen schreiben.";
-
+}
   
 }
 
